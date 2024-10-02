63275b2ed589bbd443012e1863d0b1db1655daab
lordixir <alimuratbzgl6@gmail.com>
2024-10-02 06:33:35 +0300

    added async worker

--- modules/resolve_site.go
+++ modules/resolve_site.go
@@ -1,22 +1,43 @@
-package modules
+    package modules
 
-import "fmt"
+    import (
+        "fmt"
+        "sync"
+    )
 
-func ResolveSite(IPAddress []string, Websites [][]string, DomainTitle string, IPBlocks []string, domain string, con bool, export bool, timeout int) {
-	for _, ip := range IPAddress {
-		site := GetSite(ip, domain, timeout)
-		if len(site) > 0 {
-			fmt.Print("+")
-			Websites = append(Websites, site)
+    func ResolveSite(IPAddress []string, Websites [][]string, DomainTitle string, IPBlocks []string, domain string, con bool, export bool, timeout int) {
+        var wg sync.WaitGroup
+        var mu sync.Mutex
+        sem := make(chan struct{}, 100)
 
-			if DomainTitle != "" && site[2] == DomainTitle && con == false {
-				PrintResult("Search Domain by ASN", DomainTitle, timeout, IPBlocks, Websites, export)
-				return
-			}
-		} else {
-			fmt.Print("-")
-		}
-	}
+        for _, ip := range IPAddress {
+            wg.Add(1)
+            sem <- struct{}{}
 
-	PrintResult("Search All ASN/IP", DomainTitle, timeout, IPBlocks, Websites, export)
-}
+            go func(ip string) {
+                defer wg.Done()
+                defer func() { <-sem }()
+
+                site := GetSite(ip, domain, timeout)
+                if len(site) > 0 {
+
+                    fmt.Println("\n", site)
+                    mu.Lock()
+                    Websites = append(Websites, site)
+                    mu.Unlock()
+
+                    if DomainTitle != "" && site[2] == DomainTitle && con == false {
+                        PrintResult("Search Domain by ASN", DomainTitle, timeout, IPBlocks, Websites, export)
+                        return
+                    }
+                } else {
+                    fmt.Print("-")
+                }
+            }(ip)
+        }
+
+        wg.Wait()
+
+        // Sonuçları toplu olarak işlemek için PrintResult çağrısı
+        PrintResult("Search All ASN/IP", DomainTitle, timeout, IPBlocks, Websites, export)
+    }
